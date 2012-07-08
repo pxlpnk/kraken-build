c63884e447f38f4dd2484ab2fab0c4aa7b2b3b33
at <at@an-ti.eu>
2012-07-09 01:44:58 +0200

    basic functionality,       still missing: get config from master branch use for new branch

--- lib/kraken-build.rb
+++ lib/kraken-build.rb
@@ -7,8 +7,55 @@ require "kraken-build/github-api.rb"
 
 
 
-module Kraken
-  module Build
-    # Your code goes here...
+module KrakenBuild
+
+  def self.set_config(options = {})
+    @config = options
+    @repository = options[:repository]
+    @github = GithubApi.new(@config)
+    @jenkins = JenkinsApi.new(@config)
+    @jobs = []
+    @branches = []
+
+    @config
   end
+
+  def self.get_jenkins_branches
+    @jenkins.get_jobs.map{ |job| job =~ /^#{@repository}\.(.*)$/ && $1 }.compact
+  end
+
+  def self.get_github_branches
+    @github.get_branches
+  end
+
+  def self.run
+    @jobs = get_jenkins_branches
+    @branches = get_github_branches
+
+    create = compute_jobs_to_create
+      create.map do |job|
+        job_name = "#{@repository}.#{job}"
+        puts "creating => #{job_name}"
+    #     @jenkins.create_job(job_name,"")
+    #     @jenkins.build_job(job_name)
+      end
+
+
+    remove = compute_jobs_to_remove
+    remove.map do |job|
+      job_name = "#{@repository}.#{job}"
+      puts "removing => #{job_name}"
+      @jenkins.remove_job(job_name)
+    end
+
+  end
+
+   def self.compute_jobs_to_create
+    @branches - @jobs
+  end
+
+  def self.compute_jobs_to_remove
+    @jobs - @branches
+  end
+
 end
